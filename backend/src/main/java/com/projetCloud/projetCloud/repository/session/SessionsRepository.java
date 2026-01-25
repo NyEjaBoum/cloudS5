35dd02e24deb06d0bb61e9f8e9886faafca82f5b
vatosoa-wanda <154837268+vatosoa-wanda@users.noreply.github.com>
2026-01-25 14:26:00 +0300

    auth
    - inscription et login local
    - login firebase avec token

---
+++ backend/src/main/java/com/projetCloud/projetCloud/repository/session/SessionsRepository.java
@@ -0,0 +1,8 @@
+package com.projetCloud.projetCloud.repository.session;
+
+import com.projetCloud.projetCloud.model.session.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionsRepository extends JpaRepository<Session, Long> {
+    // Add custom methods if needed, e.g., findByTokenJwt(String token)
+}
